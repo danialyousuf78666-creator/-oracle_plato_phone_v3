09b060ff634d6b33d22eca0f0534948e246ee6fa
danialyousuf78666-creator <danialyousuf78666@gmail.com>
2026-09-09 23:05:29 +1000

    Cache v3.5 prize coverage interface

--- v35/sw.js
+++ v35/sw.js
@@ -1,5 +1,12 @@
-const CACHE='plato-v35-phone-4';
-const ASSETS=['./index.html','./manifest.webmanifest','./plato_v35_phone.js'];
+const CACHE='plato-v35-phone-5';
+const ASSETS=['./index.html','./manifest.webmanifest','./plato_v35_phone.js','./prize_coverage.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('plato-v35-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.origin!==location.origin)return;if(e.request.mode==='navigate'){e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')));return;}e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{if(e.request.method==='GET'&&u.pathname.includes('/v35/')){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return res}).catch(()=>caches.match(e.request)));});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);if(u.origin!==location.origin)return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return res}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  e.respondWith(fetch(e.request,{cache:'no-store'}).then(res=>{if(e.request.method==='GET'&&u.pathname.includes('/v35/')){const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return res}).catch(()=>caches.match(e.request)));
+});
